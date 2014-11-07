9d0cfe67370a5309228531389ef6fdbf0a1598d4
Scott <sscotth@gmail.com>
2014-11-07 12:22:43 -0600

    refactor tests to use numbers and fix nesting

--- spec/test.js
+++ spec/test.js
@@ -1,52 +1,50 @@
 /* global describe, it */
 
-(function () {
+;(function(){
   'use strict';
 
   describe('Entering numbers', function(){
     describe('Entering the decimal number 7 . 8 9', function(){
       it('should display 7.89', function(){
-        press('7');
+        press(7);
         press('.');
-        press('8');
-        press('9');
-        it('should be 7.89', function () {
-          assert(displayOuput(), "10.89");
-        });
+        press(8);
+        press(9);
+        assert.equal(displayOutput(), 7.89);
       });
     });
   });
 
   describe('Adding', function () {
     describe('7 . 8 9 + 1 + 2 =', function(){
-      press('7');
-      press('.');
-      press('8');
-      press('9');
-      press('+');
-      press('1');
-      press('+');
-      press('2');
-      press('=');
       it('should be 10.89', function () {
-        assert(displayOuput(), "10.89");
+        press(7);
+        press('.');
+        press(8);
+        press(9);
+        press('+');
+        press(1);
+        press('+');
+        press(2);
+        press('=');
+        assert.equal(displayOutput(), "10.89");
       });
     });
     describe('7 . 8 9 + 1 . 0 0 + 2 =', function(){
-      press('7');
-      press('.');
-      press('8');
-      press('9');
-      press('+');
-      press('1');
-      press('.');
-      press('0');
-      press('0');
-      press('+');
-      press('2');
-      press('=');
       it('should be 10.89', function () {
-        assert(displayOuput(), "10.89");
+        press(7);
+        press('.');
+        press(8);
+        press(9');
+        press('+');
+        press(1);
+        press('.');
+        press(0);
+        press(0);
+        press('+');
+        press(2);
+        press('=');
+        assert.equal(displayOutput(), '10.89');
       });
     });
     describe('1 + 1 =', function(){
